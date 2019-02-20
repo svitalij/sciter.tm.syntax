55196ee8fe287bdc2527b18a2cd2e593f83a61d9
Vitalij Shitts <vit@conrlab.com>
2019-02-20 18:16:04 +0300

    html in yaml

--- convert.bat
+++ convert.bat
@@ -1,9 +1,9 @@
-cls
-set DEST=%~dp0..\..\syntaxes
+@echo off
+set DEST=%1
+echo "Generating in %DEST%"
 del /f %DEST%\*.json
 
 cmd /c npx js-yaml %~dp0SciterSelectors.tmLanguage.yaml > %DEST%/SciterSelectors.tmLanguage.json
 cmd /c npx js-yaml %~dp0SciterCss.tmLanguage.yaml > %DEST%/SciterCss.tmLanguage.json
 cmd /c npx js-yaml %~dp0SciterTis.tmLanguage.yaml > %DEST%/SciterTis.tmLanguage.json
-copy /y %~dp0SciterHTML.json %DEST%
-
+cmd /c npx js-yaml %~dp0SciterHTML.tmLanguage.yaml > %DEST%/SciterHTML.tmLanguage.json
